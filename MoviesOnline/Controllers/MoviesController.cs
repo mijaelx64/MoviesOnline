c562725bf94ebf42882f6d43344bd283a5cad0bf
Mijael Vargas <mijael.vargas007@gmail.com>
2017-02-18 00:48:47 -0400

    Adding Rating Field

--- MoviesOnline/Controllers/MoviesController.cs
+++ MoviesOnline/Controllers/MoviesController.cs
@@ -70,7 +70,7 @@ namespace MoviesOnline.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Create([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
@@ -102,7 +102,7 @@ namespace MoviesOnline.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price")] Movie movie)
+        public ActionResult Edit([Bind(Include = "ID,Title,ReleaseDate,Genre,Price,Rating")] Movie movie)
         {
             if (ModelState.IsValid)
             {
