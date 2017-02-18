5e82dc408ff12054b485017ebdbe5f8be10899ef
Mijael Vargas <mijael.vargas007@gmail.com>
2017-02-17 21:35:05 -0400

    Add project files.

---
+++ MoviesOnline/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MoviesOnline.MvcApplication" Language="C#" %>
---
+++ MoviesOnline/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MoviesOnline.Startup))]
+namespace MoviesOnline
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
